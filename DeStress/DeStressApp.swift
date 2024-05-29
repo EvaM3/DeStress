d6d49ec8a1b7a71e1a83bec363b65140be7600a2
EvaM3 <tora.buke@gmail.com>
2024-05-29 22:32:01 +0200

    Initial Commit

---
+++ DeStress/DeStressApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeStressApp.swift
+//  DeStress
+//
+//  Created by Eva Sira Madarasz on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeStressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
